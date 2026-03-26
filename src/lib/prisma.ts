83fb530c8475cb100dde6333ecaf51ec6048e5da
Mattias Ternoot <mattiasternoot@mac.home>
2026-03-26 22:08:43 +0100

    Add statusControlsEnabled to SchoolClass

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,7 +4,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const connectionString = process.env.DATABASE_URL;
 
 if (!connectionString) {
-  throw new Error("DATABASE_URL is niet ingesteld");
+  throw new Error("crossover.proxy.rlwy.net:10450");
 }
 
 const adapter = new PrismaPg({ connectionString });
